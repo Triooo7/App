7ae57e4b47c8ba62edbb4cabd9b98f22535b9eae
Website 1 <krishna.raju93@gmail.com>
2017-01-22 12:41:32 +0200

    Top 10 App File

---
+++ Top10Apps/app/src/main/java/com/trioo/top10apps/FeedEntry.java
@@ -0,0 +1,63 @@
+package com.trioo.top10apps;
+
+/**
+ * Created by Madhouse on 19/01/2017.
+ */
+
+public class FeedEntry {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+    private String summary;
+    private String imageURL;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return"name=" + name + '\n' +
+                ", artist=" + artist + '\n' +
+                ", releaseDate=" + releaseDate + '\n' +
+                ", imageURL=" + imageURL + '\n';
+
+    }
+}

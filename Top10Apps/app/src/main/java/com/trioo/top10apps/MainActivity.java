234c27766bc33e7fde2ac17f3fc7e3cf2b247db4
Website 1 <krishna.raju93@gmail.com>
2017-01-23 00:21:44 +0200

    top 10 app new changes

--- Top10Apps/app/src/main/java/com/trioo/top10apps/MainActivity.java
+++ Top10Apps/app/src/main/java/com/trioo/top10apps/MainActivity.java
@@ -1,10 +1,11 @@
 package com.trioo.top10apps;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import java.io.BufferedReader;
@@ -17,19 +18,66 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private ListView listApps;
+    private String feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+    private int feedLimit = 10;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        listApps =(ListView) findViewById(R.id.xmlListveiw);
+        listApps = (ListView) findViewById(R.id.xmlListveiw);
 
-        Log.d(TAG, "onCreate: starting AsyncTask");
-        DownloadData downloadData = new DownloadData();
-        downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=25/xml");
-        Log.d(TAG, "onCreate: done");
+        downloadUrl(String.format(feedUrl, feedLimit));
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.feeds_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.mnuFree:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=%d/xml";
+                break;
+            case R.id.mnuPaid:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/toppaidapplications/limit=%d/xml";
+                break;
+            case R.id.mnuSongs:
+                feedUrl = "http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topsongs/limit=%d/xml";
+                break;
+            case R.id.mnu10:
+            case R.id.mnu25:
+                if(!item.isChecked()){
+                    item.setChecked(true);
+                    feedLimit= 35-feedLimit;
+                    Log.d(TAG,"onOptionsItemSelected: "+ item.getTitle()+"setting feedLimit to" + feedLimit);
+                } else {
+                    Log.d(TAG,"onOptionsItemSelected: "+ item.getTitle()+"feedLimit unchanged");
+                }
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+
+        }
+        downloadUrl(String.format(feedUrl, feedLimit));
+        return true;
+    }
+
+    private void downloadUrl(String feedUrl) {
+        Log.d(TAG, "downloadUrl: starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute(feedUrl);
+        Log.d(TAG, "downloadUrl: done");
+
+    }
+
 
     //    creating the class extends the async task (Subclass) helps in multithreading
     private class DownloadData extends AsyncTask<String, Void, String> {
@@ -38,13 +86,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
-            Log.d(TAG, "onPostExecute: parameter is " + s);
+//            Log.d(TAG, "onPostExecute: parameter is " + s);
             ParseApplications parseApplications = new ParseApplications();
             parseApplications.parse(s);
 
-            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(
-                    MainActivity.this, R.layout.list_item, parseApplications.getApplications());
-            listApps.setAdapter(arrayAdapter);
+//            ArrayAdapter<FeedEntry> arrayAdapter = new ArrayAdapter<FeedEntry>(
+//                    MainActivity.this, R.layout.list_item, parseApplications.getApplications());
+//            listApps.setAdapter(arrayAdapter);
+            FeedAdapter feedAdapter = new FeedAdapter(MainActivity.this, R.layout.list_record,
+                    parseApplications.getApplications());
+            listApps.setAdapter(feedAdapter);
+
         }
 
         @Override
@@ -57,30 +109,30 @@ public class MainActivity extends AppCompatActivity {
             return rssFeed;
         }
 
-        private String downloadXML(String urlPath){
+        private String downloadXML(String urlPath) {
             StringBuilder xmlResult = new StringBuilder();
 
-            try{
-                URL url = new URL (urlPath);
+            try {
+                URL url = new URL(urlPath);
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-                int response =connection.getResponseCode();
+                int response = connection.getResponseCode();
                 Log.d(TAG, "downloadXML: The response code was" + response);
 //                InputStream inputStream = connection.getInputStream();
 //                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
 //                BufferedReader reader = new BufferedReader(inputStreamReader);
 //
-                BufferedReader reader =new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 //                The above line function is to check the input stream reader and then go to the connection and next to the input stream
 
 //             Here we assign it to read characters upto 500 and then read them and follow the if condition and append them to the input buffer
                 int charsRead;
                 char[] inputBuffer = new char[500];
-                while(true) {
-                    charsRead =reader.read(inputBuffer);
-                    if (charsRead<0){
+                while (true) {
+                    charsRead = reader.read(inputBuffer);
+                    if (charsRead < 0) {
                         break;
                     }
-                    if (charsRead>0){
+                    if (charsRead > 0) {
                         xmlResult.append(String.copyValueOf(inputBuffer, 0, charsRead));
                     }
                 }
@@ -90,10 +142,10 @@ public class MainActivity extends AppCompatActivity {
 
             } catch (MalformedURLException e) {
                 Log.e(TAG, "downloadXML: Invalid URL" + e.getMessage());
-            } catch (IOException e){
+            } catch (IOException e) {
                 Log.e(TAG, "downloadXML: IO Exception reading data:" + e.getMessage());
-            } catch(SecurityException e) {
-                Log.e(TAG, "downloadXML: Security Exception . Needs permission ?"+ e.getMessage());
+            } catch (SecurityException e) {
+                Log.e(TAG, "downloadXML: Security Exception . Needs permission ?" + e.getMessage());
                 e.printStackTrace();
 
             }
